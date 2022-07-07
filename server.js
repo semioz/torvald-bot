3573888ba99f2ec2e9ca9ee0bab9595814ce2e01
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-07-07 23:34:59 +0300

    olbe

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.all("/", (req, res) => {
 })
 
 function stayinAlive() {
-    server.listen(process.env.PORT || 3000, function() {
+    app.listen(process.env.PORT || 3000, function() {
         console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     })
 }
