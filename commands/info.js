4122f47ad0ec0cc04e7ac255362eb4691a2aadaa
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-07-12 12:12:07 +0300

    add new features for info.js

--- commands/info.js
+++ commands/info.js
@@ -5,10 +5,10 @@ export default {
     execute(msg) {
         const infoEmbed = new MessageEmbed()
             .setColor('#143F6B')
-            .setTitle("Commands and GitHub Repository of Torvald Bot")
+            .setTitle("Commands")
             .setURL('https://github.com/semihberkayozturk/Torvald-Bot')
             .setAuthor({ name: 'GitHub', url: 'https://github.com/semihberkayozturk/Torvald-Bot' })
-            .addFields({ name: '● $info', value: 'dene more details about Torvald Bot.' }, { name: '● $inspire', value: "Torvald Bot sends inspirational quotes  from the world's most important philosiphers, inventors, scientists, religious figures, and leaders throughout history." }, { name: '● $guess <value>', value: 'Try to guess the number which is between 1 and 10' }, { name: '● $pomodoro', value: "Torvald Bot starts a 25 minutes long Pomodoro session and sends you a message when it's finished." }, { name: '● $translate <text>', value: "With this command, Torvald Bot detects the language you've written and translates it to English." }, { name: '● $bitcoin', value: "Current price of Bitcoin." }, { name: '● $meme', value: "Torvald Bot shows programming memes." }, { name: '● $ping', value: "Ping stats of Discord and Torvald Bot." }, { name: '● $reddit <subreddit name>', value: "Random posts from a reddit subreddit you've chosen except NSFW posts." })
+            .addFields({ name: '● $info', value: 'dene more details about Torvald Bot.' }, { name: '● $inspire', value: "Torvald Bot sends inspirational quotes  from the world's most important philosiphers, inventors, scientists, religious figures, and leaders throughout history." }, { name: '● $guess <value>', value: 'Try to guess the number which is between 1 and 10' }, { name: '● $pomodoro', value: "Torvald Bot starts a 25 minutes long Pomodoro session and sends you a message when it's finished." }, { name: '● $translate <text>', value: "With this command, Torvald Bot detects the language you've written and translates it to English." }, { name: '● $bitcoin', value: "Current price of Bitcoin." }, { name: '● $meme', value: "Torvald Bot shows programming memes." }, { name: '● $ping', value: "Ping stats of Discord and Torvald Bot." }, { name: '● $reddit <subreddit name>', value: "Random posts from a reddit subreddit you've chosen." })
         msg.channel.send({ embeds: [infoEmbed] });
     }
-}
+};
