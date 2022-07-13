86f34f77fcf27f1517f71a4b1b29b2d41b12e9f4
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-07-13 12:24:29 +0300

    fix the guess command

--- commands/guess.js
+++ commands/guess.js
@@ -3,7 +3,7 @@ export default {
     execute(msg) {
         let selectedNumber = Math.floor(Math.random() * 10) + 1;
         let response = msg.content.slice((process.env.PREFIX).length).trim().split(/ +/);
-        if (response == selectedNumber) {
+        if (response[1] == selectedNumber) {
             msg.channel.send(`Congrats ${msg.author} ! You've selected the correct number.`);
         } else  {
             msg.channel.send("Ooops ! You couldn't the find the correct number ❌ It was " + selectedNumber)
