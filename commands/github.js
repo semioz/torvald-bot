a54e4ac9af837243a9bd778ca03e1d3f42122810
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-07-18 11:40:38 +0300

    fix the date format in github command

--- commands/github.js
+++ commands/github.js
@@ -15,7 +15,7 @@ export default {
                     .setTitle(data["name"])
                     .setThumbnail(data["avatar_url"])
                     .setDescription(data["bio"])
-                    .addFields({ name: "● Followers", value: data["followers"].toString(), }, { name: "● Following", value: data["following"].toString() }, { name: "● Creation Date", value: data["created_at"] }, { name: "● Public Repositories", value: data["public_repos"].toString() }, { name: "● Public Gists", value: data["public_gists"].toString() })
+                    .addFields({ name: "● Followers", value: data["followers"].toString(), }, { name: "● Following", value: data["following"].toString() }, { name: "● Creation Date", value: data["created_at"].slice(0, 10) }, { name: "● Public Repositories", value: data["public_repos"].toString() }, { name: "● Public Gists", value: data["public_gists"].toString() })
                     .setTimestamp()
                     .setURL(`https://github.com/${values}`)
                 msg.reply({ embeds: [accountEmbed] });
