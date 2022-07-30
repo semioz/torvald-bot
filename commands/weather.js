e41a47e8f4caffe1b722b2284e2d79fe7286610d
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-07-30 20:20:50 +0300

    fix the 2 words city problem

--- commands/weather.js
+++ commands/weather.js
@@ -4,7 +4,8 @@ import { MessageEmbed } from "discord.js";
 export default {
     name: "weather",
     execute(msg) {
-        let city = (msg.content.slice((process.env.PREFIX).length).trim().split(/ +/))[1];
+        let element = (msg.content.slice((process.env.PREFIX).length).trim().split(/ +/));
+        let city = element.slice(1, element.length - 1).join(' ') + " " + element.slice(-1)
         return fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + process.env.API_KEY + "&units=metric")
             .then(res => {
                 return res.json()
@@ -12,7 +13,7 @@ export default {
             .then(data => {
                 const weatherEmbed = new MessageEmbed()
                     .setColor('#143F6B')
-                    .setTitle(city.charAt(0).toUpperCase() + city.slice(1))
+                    .setTitle(city.toUpperCase())
                     .setThumbnail(`http://openweathermap.org/img/wn/` + data["weather"][0].icon + `@2x.png`)
                     .setDescription(data["sys"].country)
                     .addFields({ name: `● Weather`, value: (data["weather"][0].main).toString() }, { name: `● Celcius`, value: Math.round(data["main"].feels_like).toString() })
