576e6d6f5b3b2886218c446ddb9f60ed6c8803d0
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-08-26 16:45:55 +0300

    implement uuid for message id's

--- commands/fback.js
+++ commands/fback.js
@@ -1,9 +1,11 @@
+import { v4 as uuidv4 } from "uuid";
+
 export default {
     name: "fback",
     description: "Give feedback to the developer of Torvald Bot",
     async execute(msg) {
         let value = msg.content.slice(7)
-        await msg.client.database.fetch((Math.floor(Math.random() * 2147483647)).toString(), value, msg.author)
+        await msg.client.database.fetch(uuidv4(), value, msg.author)
         await msg.channel.send(`${msg.author} Thank you for the feedback. My developer will work on it.`)
     }
 };
