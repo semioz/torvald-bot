5dc96a69d9257b4bb5137f0a787f948a070acea6
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-09-14 16:52:44 +0300

    add date to the schema

--- database/message_schema.js
+++ database/message_schema.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 let firstSchema = new mongoose.Schema({
     message_id: { type: String, unique: true, required: true },
     message: { type: String, default: "No Message" },
-    username: { type: String, required: true }
+    username: { type: String, required: true },
+    date: { type: Date, default: new Date().toJSON().slice(0, 10).replace(/-/g, '/') }
 })
 
 let Message = mongoose.model("Message", firstSchema)
