9c8c79ee939bb40c078b2e386904d3f317acf955
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2022-10-29 12:24:59 +0300

    implement redis hset and expire

--- commands/get.js
+++ commands/get.js
@@ -1,5 +1,8 @@
 import redis from "redis";
-const redisClient = redis.createClient(process.env.REDIS_URL)
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL
+});
+redisClient.on("error", (err) => console.log("Redis Client Error:", err));
 await redisClient.connect();
 
 //TODO: SEND ALL THE MESSAGES OF USER.
@@ -8,17 +11,21 @@ export default {
     name: "get",
     description: "Get the message from the database via Redis or MongoDB",
     async execute(msg) {
+        let firstTime = Date.now();
         let value = msg.content.slice(5)
-        const cachedMessages = await redisClient.get(value)
+        let hashKey = "";
+
+        const cachedMessages = await redisClient.hGet(hashKey, value);
         if (cachedMessages) {
             console.log("Serving from cache!")
-            return msg.reply(JSON.parse(cachedMessages))
+            return msg.reply(`Speed:${Date.now()-firstTime} ${JSON.parse(cachedMessages)}`)
         }
         let message = await msg.client.database.fetch(value)
-        await console.log("Serving from MongoDB!")
+        console.log("Serving from MongoDB!")
+        await msg.reply(`Speed: ${Date.now()-firstTime} ${message["message"]}`)
 
-        await msg.reply(message["message"])
-
-        redisClient.set(value, JSON.stringify(message["message"]))
+        redisClient.hSet(hashKey, value, JSON.stringify(message["message"]))
+            //After 10 seconds, value expires and redis removes it from the cache.
+        redisClient.expire(hashKey, 10)
     }
 };
